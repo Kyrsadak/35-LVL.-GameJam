2dca98fe9c39cdce15681fb0d42f4e23284b4468
Abdujamil <aydarbekovabdujamil1406gmail.com@MacBook-Air-Abdujamil.local>
2026-08-22 21:05:40 +0500

    tune: decrease robot movement speed by 30% for more deliberate control

--- PROJECT_KOSYLGAN/characters/robots/RobotBase.gd
+++ PROJECT_KOSYLGAN/characters/robots/RobotBase.gd
@@ -9,9 +9,9 @@ signal interact_target_changed(target: Node)
 @export var robot_display_name: String = "ROBOT"
 @export var robot_color: Color = Color.WHITE
 
-@export var move_speed: float = 6.8
-@export var acceleration: float = 26.0
-@export var friction: float = 28.0
+@export var move_speed: float = 4.76
+@export var acceleration: float = 18.2
+@export var friction: float = 20.0
 @export var gravity: float = 25.0
 
 @export var max_battery: float = 100.0
