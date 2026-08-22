85fa1fa9ec0276a122bfb8df648e80b33294f909
Abdujamil <aydarbekovabdujamil1406gmail.com@MacBook-Air-Abdujamil.local>
2026-08-22 21:38:32 +0500

    fix: correctly tilt security camera downward at the floor and redesign wall fan into proper industrial turbine vent

--- PROJECT_KOSYLGAN/scenes/props/prop_wall_fan.gd
+++ PROJECT_KOSYLGAN/scenes/props/prop_wall_fan.gd
@@ -1,9 +1,9 @@
 class_name WallFan
 extends Node3D
 
-@export var rotation_speed: float = 4.0
-@onready var blades: Node3D = $Housing/Blades
+@export var rotation_speed: float = 3.5
+@onready var turbine_blades: Node3D = $VentFrame/Turbine
 
 func _process(delta: float) -> void:
-	if blades:
-		blades.rotation.z += rotation_speed * delta
+	if turbine_blades:
+		turbine_blades.rotation.z += rotation_speed * delta
