4e8d44bdba2b792e81fee3dca68ab85f8ffb3c0e
Abdujamil <aydarbekovabdujamil1406gmail.com@MacBook-Air-Abdujamil.local>
2026-08-23 02:19:54 +0500

    chore: clean up project workspace by removing unused demo builds, duplicate skin folders, and temporary backup files

--- демоигры/top-down-action-adventure-starter-kit-main/characters/enemies/melee/minion/attack_player.gd
+++
@@ -1,8 +0,0 @@
-extends ActionLeaf
-
-
-func tick(actor, blackboard):
-	actor.move_to_idling()
-	actor.current_state = actor.BehaviorState.Attacking
-	actor.play_on_attacking(true)
-	return SUCCESS
--- демоигры/top-down-action-adventure-starter-kit-main/characters/enemies/melee/minion/player_is_in_reach.gd
+++
@@ -1,8 +0,0 @@
-class_name PlayerIsInReach extends ConditionLeaf
-
-
-func tick(actor, blackboard):
-	if actor.is_target_in_reach:
-		return SUCCESS
-	else:
-		return FAILURE
