5331f00601801667c025a2b74fd11484c4c35628
Abdujamil <aydarbekovabdujamil1406gmail.com@MacBook-Air-Abdujamil.local>
2026-08-23 06:53:02 +0500

    revert(audio): restore previous warm mechanical tap footstep sound

--- PROJECT_KOSYLGAN/characters/robots/RobotBase.gd
+++ PROJECT_KOSYLGAN/characters/robots/RobotBase.gd
@@ -166,13 +166,13 @@ func _handle_movement(delta: float) -> void:
 		var current_speed_scale = 0.75
 		if skin and "anim_player" in skin and skin.anim_player:
 			current_speed_scale = skin.anim_player.speed_scale
-		var step_interval = 0.50 / max(0.1, current_speed_scale)
+		var step_interval = 0.38 / max(0.1, current_speed_scale)
 
 		_step_timer += delta
 		if _step_timer >= step_interval:
 			_step_timer = 0.0
 			if SoundManager:
-				SoundManager.play_footstep(-4.0)
+				SoundManager.play_footstep(-6.0)
 	else:
 		_step_timer = 0.20
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
